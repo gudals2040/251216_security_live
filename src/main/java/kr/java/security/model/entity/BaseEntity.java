ef753fa453c51ec5315b1d1725ff2bfde1b61976
PARK HYEONG MIN <qd0420@gmail.com>
2025-12-16 15:14:01 +0900

    step03

--- src/main/java/kr/java/security/model/entity/BaseEntity.java
+++ src/main/java/kr/java/security/model/entity/BaseEntity.java
@@ -2,7 +2,6 @@ package kr.java.security.model.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
